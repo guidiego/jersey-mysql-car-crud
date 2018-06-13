1c75ba0a33af3ccecfbcc01c03fdb50c4681b4d6
Guilherme Diego <guidiego.expgames@gmail.com>
2018-06-13 04:54:11 -0300

    End: Base API

--- api/src/main/java/br/com/carlist/car/Endpoint.java
+++ api/src/main/java/br/com/carlist/car/Endpoint.java
@@ -1,35 +1,21 @@
 package br.com.carlist.car;
 
 import javax.ws.rs.GET;
-// import javax.ws.rs.POST;
-// import javax.ws.rs.PUT;
-
-// import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-
-// import javax.ws.rs.DELETE;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.Path;
-// import javax.ws.rs.PathParam;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-// import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.Response.Status;
 
 
-// class Person {
-//     public int id;
-//     public String name;
-
-//     public Person() {
-
-//     }
-
-//     public Person(int i, String n) {
-//         this.name = n;
-//         this.id = i;
-//     }
-// }
 
 @Path("/car")
 public class Endpoint {
@@ -39,45 +25,67 @@ public class Endpoint {
     @Path("/")
     @Produces(MediaType.APPLICATION_JSON)
     public Response find() {
-        List<Car> cars = new ArrayList<Car>();
+        System.out.println("[LOG] Endpoint.select");
 
         try {
-            cars = this.service.select();
+            return Response.ok(
+                this.service.select()
+            ).build();
         } catch (Exception e) {
-            System.out.println("Trouble: " + e);
+            System.out.println("[ERROR] Endpoint.select:" + e);
+            return Response.status(Status.INTERNAL_SERVER_ERROR).build();
         }
-
-        return Response.ok(cars).build();
     }
 
-    // @POST
-    // @Path("/")
-    // @Produces(MediaType.APPLICATION_JSON)
-    // public Car create(Car c) throws SQLException {
-    //     this.service.save(c);
-    //     return c;
-    // }
+    @POST
+    @Path("/")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response create(Car input) {
+        System.out.println("[LOG] Endpoint.update: NAME=" +
+            input.name + " TYPE=" + input.type);
 
-    // @PUT
-    // @Path("/")
-    // @Produces(MediaType.APPLICATION_JSON)
-    // public Car update(Car c) throws SQLException {
-    //     this.service.save(c);
-    //     return c;
-    // }
+        try {
+            return Response.ok(
+                this.service.insert(input)
+            ).build();
+        } catch (Exception e) {
+            System.out.println("[ERROR] Endpoint.create:" + e);
+            return Response.status(Status.INTERNAL_SERVER_ERROR).build();
+        }
+    }
 
-    // @GET
-    // @Path("/{id}")
-    // @Produces(MediaType.APPLICATION_JSON)
-    // public Car findOne(@PathParam("id") Integer id) throws SQLException {
-    //     return this.service.findOne(id);
-    // }
+    @PUT
+    @Path("/")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response update(Car input) {
+        System.out.println("[LOG] Endpoint.update: ID=" + input.id);
+        System.out.println("[LOG] Endpoint.update: NAME=" +
+            input.name + " TYPE=" + input.type);
 
-    // @DELETE
-    // @Path("/{id}")
-    // @Produces(MediaType.TEXT_PLAIN)
-    // public String delete(@PathParam("id") Integer id) throws SQLException {
-    //     this.service.remove(id);
-    //     return "ok";
-    // }
+        try {
+            return Response.ok(
+                this.service.update(input)
+            ).build();
+        } catch (Exception e) {
+            System.out.println("[ERROR] Endpoint.update:" + e);
+            return Response.status(Status.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response delete(@PathParam("id") Integer id) {
+        System.out.println("[LOG] Endpoint.delete: ID=" + id);
+
+        try {
+            this.service.delete(id);
+            Map<String, String> resp = new HashMap<String, String>();
+            resp.put("id", id.toString());
+            return Response.ok(resp).build();
+        } catch (Exception e) {
+            System.out.println("[ERROR] Endpoint.delete:" + e);
+            return Response.status(Status.INTERNAL_SERVER_ERROR).build();
+        }
+    }
 }
--- api/src/main/java/br/com/carlist/car/Service.java
+++ api/src/main/java/br/com/carlist/car/Service.java
@@ -4,6 +4,9 @@ package br.com.carlist.car;
 import java.util.ArrayList;
 import java.util.List;
 
+import java.sql.PreparedStatement;
+import com.mysql.jdbc.Statement;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -33,11 +36,51 @@ public class Service {
         while (rs.next()) {
             list.add(new Car(
                 Integer.parseInt(rs.getString("id")),
-                rs.getString("nome"),
-                rs.getString("tipo")
+                rs.getString("name"),
+                rs.getString("type")
             ));
         }
 
         return list;
+    }
+
+    public Car insert(Car car) throws SQLException{
+        PreparedStatement ps = this.connection
+            .prepareStatement(
+                "insert into cars (name, type) values (?, ?)",
+                Statement.RETURN_GENERATED_KEYS
+            );
+
+        ps.setString(1, car.name);
+        ps.setString(2, car.type);
+        ps.executeUpdate();
+
+        ResultSet rs = ps.getGeneratedKeys();
+        rs.next();
+        car.id = rs.getInt(1);
+
+        return car;
+    }
+
+    public Car update(Car car) throws SQLException {
+        PreparedStatement ps = this.connection
+            .prepareStatement(
+                "update cars set name = ?, type = ? where id = ?"
+            );
+
+        ps.setString(1, car.name);
+        ps.setString(2, car.type);
+        ps.setInt(3, car.id);
+        ps.executeUpdate();
+
+        return car;
+    }
+
+    public void delete(Integer id) throws SQLException{
+        PreparedStatement ps = this.connection
+            .prepareStatement("delete from cars where id = ?");
+
+        ps.setInt(1, id);
+        ps.executeUpdate();
     }
 }

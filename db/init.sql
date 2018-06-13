64616e5c2fe1fe05ea209a5f1f652559037691b2
Guilherme Diego <guidiego.expgames@gmail.com>
2018-06-13 05:07:58 -0300

    Improve: Infraesctructure

--- db/init.sql
+++ db/init.sql
@@ -1,8 +1,9 @@
-CREATE DATABASE cars_crud;
-USE cars_crud;
+-- CREATE DATABASE cars_crud;
+-- USE cars_crud;
 
 CREATE TABLE cars (
-    id int not null,
+    id int not null AUTO_INCREMENT,
     name varchar(50) not null,
-    type varchar(50) not null
+    type varchar(50) not null,
+    PRIMARY KEY (id)
 )

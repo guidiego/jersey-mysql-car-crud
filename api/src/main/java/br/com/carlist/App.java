c360bbccb0a8ead872dba7183245eab80a37ce30
Guilherme Diego <guidiego.expgames@gmail.com>
2018-06-13 14:33:08 -0300

    Add: GitIgnore stuff and CORS to api

--- api/src/main/java/br/com/carlist/App.java
+++ api/src/main/java/br/com/carlist/App.java
@@ -1,10 +1,16 @@
 package br.com.carlist;
 
 import br.com.carlist.car.Endpoint;
+
+import java.util.EnumSet;
+
+import javax.servlet.DispatcherType;
+
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.FilterHolder;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
-
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -19,6 +25,13 @@ public class App {
 
         // Tells the Jersey Servlet which REST service/class to load.
         jerseyServlet.setInitParameter("jersey.config.server.provider.classnames", Endpoint.class.getCanonicalName());
+
+        // Add the filter, and then use the provided FilterHolder to configure it
+        FilterHolder cors = context.addFilter(CrossOriginFilter.class, "/*", EnumSet.of(DispatcherType.REQUEST));
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
+        cors.setInitParameter(CrossOriginFilter.ACCESS_CONTROL_ALLOW_ORIGIN_HEADER, "*");
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "GET,POST,PUT,DELETE,HEAD");
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_HEADERS_PARAM, "X-Requested-With,Content-Type,Accept,Origin");
 
         try {
             jettyServer.start();
